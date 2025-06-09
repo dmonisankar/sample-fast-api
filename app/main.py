6e600605a34b2de8f136b0ab536ecd2033567fa0
Doron Holan <doron@pay-i.com>
2025-06-09 16:05:27 -0700

    undo changes

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from app.routes import router
-# from app.database import init_db
+from app.database import init_db
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # await init_db()  # Initialize MongoDB indexes
+    await init_db()  # Initialize MongoDB indexes
     yield  # Let the application run
     print("Shutting down...")  # (Optional) Cleanup actions if needed
 
--- app/services/tools/prebuilt_tools.py
+++ app/services/tools/prebuilt_tools.py
@@ -1,20 +1,20 @@
-# from langchain_community.tools import YouTubeSearchTool
-# from langchain_community.utilities import OpenWeatherMapAPIWrapper
-# from langchain_core.tools import Tool
-# from app.config import OPENWEATHERMAP_API_KEY
+from langchain_community.tools import YouTubeSearchTool
+from langchain_community.utilities import OpenWeatherMapAPIWrapper
+from langchain_core.tools import Tool
+from app.config import OPENWEATHERMAP_API_KEY
 
-# weather = OpenWeatherMapAPIWrapper(openweathermap_api_key=OPENWEATHERMAP_API_KEY)
+weather = OpenWeatherMapAPIWrapper(openweathermap_api_key=OPENWEATHERMAP_API_KEY)
 
-# weather_search = Tool(
-#     name="weather_search",
-#     description="Get weather for a city and country code, e.g. Athens, GR",
-#     func=weather.run,
-# )
+weather_search = Tool(
+    name="weather_search",
+    description="Get weather for a city and country code, e.g. Athens, GR",
+    func=weather.run,
+)
 
-# youtube = YouTubeSearchTool()
+youtube = YouTubeSearchTool()
 
-# youtube_search = Tool(
-#     name="youtube_search",
-#     description="Search YouTube for video links.",
-#     func=youtube.run,
-# )
+youtube_search = Tool(
+    name="youtube_search",
+    description="Search YouTube for video links.",
+    func=youtube.run,
+)
